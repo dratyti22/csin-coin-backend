a8baa94f6d58edaaa6d97891135164984d547517
dratyti22 <belousovi783@gmail.com>
2024-09-07 09:39:50 +0300

    пополнение баланса

--- app/wallet/urls.py
+++ app/wallet/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from app.wallet.views import TopUpBalanceView
+
+urlpatterns = [
+    path("balance", TopUpBalanceView.as_view(), name="balance"),
 ]
--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+User = get_user_model()
+
+
+class TopUpBalanceView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        pk = request.data.get("id")
+        amount = request.data.get("amount")
+        message = request.data.get("message")
+        user = User.objects.get(id=pk)
+        user.balance += amount
+        user.save()
+        return Response({"success": "Balance replenishment completed", "message": message}, status=status.HTTP_200_OK)
