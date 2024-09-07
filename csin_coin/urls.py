16c6212f3f772eb74ec65eea27e19c8bd135a624
dratyti22 <belousovi783@gmail.com>
2024-09-07 09:25:29 +0300

    создан приложения кошелек

--- csin_coin/urls.py
+++ csin_coin/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
   path('admin/', admin.site.urls),
   path("user/", include("app.user.urls")),
+  path("", include("app.wallet.urls")),
 ] + debug_toolbar_urls()
