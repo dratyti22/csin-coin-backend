3e45803570cfd2af8a529a469f5c2e55e71bf1dc
dratyti22 <belousovi783@gmail.com>
2024-09-07 09:44:35 +0300

    изменение статуса пользователя

--- app/wallet/urls.py
+++ app/wallet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.wallet.views import TopUpBalanceView
+from app.wallet.views import TopUpBalanceView, StatusAdminView
 
 urlpatterns = [
-    path("balance", TopUpBalanceView.as_view(), name="balance"),
+    path("balance/", TopUpBalanceView.as_view(), name="balance"),
+    path("status/", StatusAdminView.as_view(), name="status"),
 ]
--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
@@ -11,6 +12,7 @@ User = get_user_model()
 class TopUpBalanceView(APIView):
     permission_classes = [IsAdminUser]
 
+    @transaction.atomic
     def post(self, request):
         pk = request.data.get("id")
         amount = request.data.get("amount")
@@ -19,3 +21,16 @@ class TopUpBalanceView(APIView):
         user.balance += amount
         user.save()
         return Response({"success": "Balance replenishment completed", "message": message}, status=status.HTTP_200_OK)
+
+
+class StatusAdminView(APIView):
+    permission_classes = [IsAdminUser]
+
+    @transaction.atomic
+    def post(self, request):
+        pk = request.data.get("id")
+        status_user = request.data.get("status")
+        user = User.objects.get(id=pk)
+        user.status = status_user
+        user.save()
+        return Response({"success": "Balance replenishment completed"}, status=status.HTTP_200_OK)
