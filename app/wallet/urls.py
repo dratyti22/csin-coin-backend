ebbf20b4568efb7695095733fa3174a62661b83e
dratyti22 <belousovi783@gmail.com>
2024-09-07 09:58:00 +0300

    перевести деньги

--- app/wallet/urls.py
+++ app/wallet/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app.wallet.views import TopUpBalanceView, StatusAdminView
+from app.wallet.views import TopUpBalanceView, StatusAdminView, TransferMoneyView
 
 urlpatterns = [
     path("balance/", TopUpBalanceView.as_view(), name="balance"),
     path("status/", StatusAdminView.as_view(), name="status"),
+    path("transfer_money/", TransferMoneyView.as_view(), name="money"),
 ]
--- app/wallet/views.py
+++ app/wallet/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from app.wallet.models import Transaction
+
 User = get_user_model()
 
 
@@ -34,3 +36,43 @@ class StatusAdminView(APIView):
         user.status = status_user
         user.save()
         return Response({"success": "Balance replenishment completed"}, status=status.HTTP_200_OK)
+
+
+class TransferMoneyView(APIView):
+    @transaction.atomic
+    def post(self, request):
+        try:
+            sender_id = request.data.get('sender_id')
+            receiver_id = request.data.get('receiver_id')
+            amount = request.data.get('amount')
+
+            sender = User.objects.get(id=sender_id)
+            receiver = User.objects.get(id=receiver_id)
+
+            if sender.status == "frozen" or receiver.status == "frozen":
+                return Response(
+                    {"failed": "You will be able to trade only after a few days"},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            if sender.status == "blocked" or receiver.status == "blocked":
+                return Response(
+                    {"failed": "You are blocked, you cannot do anything, to remove please contact the administration"},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+            # Проверка баланса отправителя
+            if sender.balance < amount:
+                raise ValueError("Недостаточно средств")
+
+            # Выполнение транзакции
+            sender.balance -= amount
+            sender.save()
+
+            receiver.balance += amount
+            receiver.save()
+
+            # Создание записи о транзакции
+            Transaction.objects.create(sender=sender, receiver=receiver, amount=amount)
+
+            return Response("Деньги переведены успешно", status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(f"Ошибка: {str(e)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
